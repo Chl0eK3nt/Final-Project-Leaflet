71eeff0100ddfdfa80c62b13dc18014c9d03a339
Chl0eK3nt <45670198+Chl0eK3nt@users.noreply.github.com>
2020-07-27 16:34:46 -0400

    yeet

--- java.js
+++ java.js
@@ -1,120 +1,138 @@
 // creates the leaflet map variable and sets the center of the map to my friend's property
 var map = L
     .map('map')
-    .setView([36.500928, -79.991763], 15);
+    .setView([36.500523, -79.993362], 15);
 
 // Add ESRI world streetmap tiles layer
 var ESRI = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
 attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map); 
 
-// Add historical layer
-var Historical = L.tileLayer('./historicaltiles/{z}/{x}/{y}.png', {
+// Add historical layer from 2010
+var Historical = L.tileLayer('./orthoimagery/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="http://osm.org/copyright">Historical</a> contributors'
 }).addTo(map);
-// create custom icons!
-var ATA = L.icon({
-iconUrl: 'https://www.themilitaryplace.com/wp-content/uploads/2016/10/atasm-1000x1000.png?x68946',
-iconSize: [80, 80], // size of the icon
+
+//add polygon representing property boundaries
+ var polygonPoints = [
+        [36.5003271, -79.9901973],
+        [36.5006751, -79.9913436],
+        [36.5006775, -79.9916075],
+        [36.5022402, -79.9931612],
+        [36.5027061, -79.9950958],
+        [36.4990092, -79.9951587],
+        [36.4988503, -79.9914422]
+ ];
+var poly = L.polygon(polygonPoints).addTo(map);
+
+// add line representing the main dirt road
+var toUnion = [[36.501261, -79.993594],
+               [36.500462, -79.992969],
+               [36.500679, -79.991294],
+               [36.499906, -79.989058],
+               [36.498620,-79.988327]];
+var road = L.polyline(toUnion,{color:'red',opacity:1}).addTo(map);
+
+// Creates marker and popup for the cabin
+var cabin = L.icon({
+iconUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/0e/b8/d8/be/outside-the-cabin-at.jpg',
+iconSize: [30, 30], 
 popupAnchor: [0,-15]
 });
-var spitfire = L.icon({
-iconUrl: 'https://i.ibb.co/yRzhzxB/plane.png', // icon url
-iconSize: [70, 70], // size of the icon
-popupAnchor: [0,-15] // where the popup should go
-});
-var royalcrown = L.icon({
-iconUrl: 'https://images.vexels.com/media/users/3/151208/isolated/preview/a61b4830c6f330b4eb2a8db3a99ebc38-royal-crown-icon-by-vexels.png', // icon url
-iconSize: [50, 50], // size of the icon
-popupAnchor: [-15,0] // where the popup should go
-});
-var star = L.icon({
-iconUrl: 'https://img.icons8.com/plasticine/2x/star.png', // icon url
-iconSize: [50, 50], // size of the icon
-popupAnchor: [0,-15] // where the popup should go
-});
-//MARKER 1 & POPUP 1
-// create popup contents for popup 1
-var customPopup1 = "<center><b>The Air Transport Auxiliary (ATA)</b> was a British civilian organisation set up during the Second World War and headquartered at White Waltham Airfield that ferried new, repaired and damaged military aircraft between factories, assembly plants, transatlantic delivery points, maintenance units (MUs), scrap yards, and active service squadrons and airfields, but not to naval aircraft carriers. It also flew service personnel on urgent duty from one place to another and performed some air ambulance work. Notably, some of its pilots were women, and from 1943 they received equal pay to their male co-workers, a first for the British government.<br/><img src='https://i.ibb.co/7JSS0H2/Q-Women-Pilots5.jpg' alt='ATA Pilot' width='200px'/></center>";
-// specify popup options for popup 1
-var customOptions1 =
+var cabpop = "<center><b>The Cabin</b><br>Built in 2015, the cabin does not show up on historic orthoimagery. It is a one room cabin with no utilities and a single bed, built for camping and storage of tools and materials.<br/><img src='https://i.pinimg.com/originals/cd/92/6a/cd926a092edb49c2a1588c4a3316d1e7.jpg' alt='The Cabin' width='200px'/></center>";
+var cabopt =
 {
 'maxWidth': '500',
 'className' : 'custom1'
 }
-// add marker, define custom icon as option, assign content and options to the popup, and add to map
-var marker1 = L.marker([51.498163, -0.773273], {icon: ATA}).bindPopup(customPopup1,customOptions1).addTo(map);
-//MARKER 2 & POPUP 2
-// create popup contents for popup 2
-var customPopup2 = "<center><b>Runway 21</b><br>White Waltham (EGLM) has three runways. Did you know that the name of the runway changes, depending on which way you are landing or taking off? An approach at this end would make this Runway 21 (pronounced two-one, not twenty-one). The runways on White Waltham were paved during WWII, but have returned to grass (soft) runways today. <br/><img src='https://i.ibb.co/yk34rV1/runway21.png' alt='ATA Pilot' width='200px'/></center>";
-// specify popup options 2
-var customOptions2 =
+var cabinn = L.marker([36.5012880, -79.9936034], {icon: cabin}).bindPopup(cabpop,cabopt).addTo(map);
+
+//creates marker and popup for northern neighbor
+var mayo = L.icon({
+iconUrl: 'https://www.townofmayodan.com/wp-content/uploads/2015/10/IMG_04831-e1446239155980.jpg', 
+iconSize: [70, 40], 
+popupAnchor: [0,-15] 
+});
+var mayopop = "<center><b>The Northern Neighbor</b><br>The land north of my friend's property has changed quite a bit since the historic orthoimagery. The trees were clear cut in 2017, and the parcel was subsequently sold to the United States government. Although currently unused, the government plans to restore this land and incorporate it into Mayo River State Park. <br/><img src='https://www.thoughtco.com/thmb/R-IsIIgh-_E7fxAnLHovq6uZjh4=/1884x1413/smart/filters:no_upscale()/ariel-view-of-trees-being-cut-down-in-the-pine-forest-1072438388-4ff7dd4154674294a028b806facc69fb.jpg' alt='Clear-cut trees' width='200px'/></center>";
+var mayoopt =
 {
 'maxWidth': '500',
 'className' : 'custom2'
 }
-var marker2 = L.marker([51.503929, -0.768647], {icon: spitfire}).bindPopup(customPopup2,customOptions2).addTo(map);
-//MARKER 3 & POPUP 3
-// create popup contents for popup 3
-var customPopup3 = "<center><b>Royal History</b><br>Prince Philip, Duke of Edinburgh was taught to fly at White Waltham in 1952, flying a De Havilland Chipmunk belonging to HQ Home Command Communications Squadron of the Royal Air Force.<br><img src='https://i.insider.com/5dee6a06fd9db248962f8afc?width=1100&format=jpeg&auto=webp' alt='popup3' width='200px'/></center>";
-// specify popup options
-var customOptions3 =
-{
-'maxWidth': '500',
-'className' : 'custom3'
-}
-var marker3 = L.marker([51.495904, -0.775047], {icon: royalcrown}).bindPopup(customPopup3,customOptions3).addTo(map);
-//MARKER 4 & POPUP 4
-// create popup contents for popup 4
-var customPopup4 = "<center><b>Aircraft Tiedowns</b><br>These spherical shapes can be seen all over aerial photos of airfields during WWII. They were blast shields built up around the aircraft tie-down to protect the aircraft.<br><img src='https://lh5.googleusercontent.com/proxy/dUO4SbFGzsHUcllmNJx5QD4uoeDjNM5k9cYECv9_JiArbfYVYgDqARMd3udz5xDj2JbTcGMvL7N_yA84d8ee_2c6bSDH3syR60b4G8uiVdy0nuGODnjvEA' alt='popup4' width='200px'/></center>";
-// specify popup options
-var customOptions4 =
+var mayoo = L.marker([36.505404,-79.994410], {icon: mayo}).bindPopup(mayopop,mayoopt).addTo(map);
+
+//creates marker and popup for dead neighbor
+var dead = L.icon({
+iconUrl: 'https://ap.rdcpix.com/c914d91f95055686804e5f2a23623c0el-m187742026xd-w1020_h770_q80.jpg', 
+iconSize: [30, 30], 
+popupAnchor: [0,-15] 
+});
+var deadpop = "<center><b>The Southern Neighbor</b><br>This property was owned by an old lady during historical imagery. However she has since passed, and her property is for sale. Today, the grass on her property is overgrown and full of small trees--this change is visible on recent satellite imagery.<br><img src='https://ssl.cdn-redfin.com/photo/150/mbphoto/195/genMid.975195_29_2.jpg' alt='Overgrown house' width='200px'/></center>";
+var deadopt =
 {
 'maxWidth': '500',
 'className' : 'custom4'
 }
-var marker4 = L.marker([51.49635, -0.78082], {icon: star}).bindPopup(customPopup4,customOptions4).addTo(map);
-//MARKER 5 & POPUP 5
-// create popup contents for popup 5
-var customPopup5 = "<center><b>Maidenhead Office Park</b><br>Then: fields. Now? An office park! What was once just fields is now bustling with activity.<br><img src='https://i.ibb.co/hy00CPp/Maidenhead-Office-Park.jpg' alt='popup4' width='200px'/></center>";
-// specify popup options
-var customOptions5 =
+var deadd = L.marker([36.499266, -79.988309], {icon: dead}).bindPopup(deadpop,deadopt).addTo(map);
+
+//creates marker and popup for cemetary
+var cemetary = L.icon({
+iconUrl: 'https://www.citynews1130.com/wp-content/blogs.dir/sites/9/2018/12/05/iStock-1006480588.jpg', 
+iconSize: [30, 30], 
+popupAnchor: [0,-15] 
+});
+var cempop = "<center><b>The Cemetary</b><br>This cemetary is located on the road up to my friend's property. It contains graves from all the way back to the 1700's.<br><img src='https://lh3.googleusercontent.com/proxy/nawQ7otDHSoHv-c24ns1qoMMaNZ_hI6Tb-GFSdyIWyikRHZPDSdT-Vf9kWlaO2FiMRbCWKGZmVLQaP0ajATWiS_rZimHBpjpLjVYV5Q_6vI13VpTALVR4Q4mG2_mAxOuFojVEcT72zgXkPHyvqCMdQO7OVua0D1o9jhlnqmnRVI50o9IVQqVZa52QeNMVeUHTloJChG21fIKLva8Qf1UNQ3gAB65dTU-mRwq6RE5TigwNE1V1_1ip376_A' alt='Cemetary' width='200px'/></center>";
+var cemopt =
 {
 'maxWidth': '500',
-'className' : 'custom5'
+'className' : 'custom4'
 }
-var marker5 = L.marker([51.50127, -0.77429], {icon: star}).bindPopup(customPopup5,customOptions5).addTo(map);
-//MARKER 6 & POPUP 6
-// create popup contents for popup 6
-var customPopup6 = "<center><b>Waltham Road</b><br>Maidenhead, the city where White Waltham Airfield is located, is far more developed than it was during WWII. It's interesting to see that this main road has pretty much remained the same. It's now an expensive road to live on, with average house prices being around £740,000 in 2019. If taking public transportation, Maidenhead is about one hour away from central London, but only thirty minutes from Windsor Castle, where the Queen of England spends a considerable amount of time. Maidenhead is a lovely town, full of history and in a great location. Pictured: Maidenhead Railway Bridge.<br><img src='https://i.ibb.co/XbNG7QD/maidenheadrailwaybridge.jpg' alt='popup4' width='200px'/></center>";
-// specify popup options
-var customOptions6 =
+var cemetaryy = L.marker([36.500230, -79.989672], {icon: cemetary}).bindPopup(cempop,cemopt).addTo(map);
+
+//creates marker for deer camp
+var deer = L.icon({
+iconUrl: 'https://tpwd.texas.gov/huntwild/wild/game_management/deer/images/WTD-Headshot.jpg', 
+iconSize: [30, 30], 
+popupAnchor: [0,-15] 
+});
+var deerpop = "<center><b>The Deer Camp</b><br>This is where the deer camp is located, where my friend hunts for food.<br><img src='https://feedbandit.com/wp-content/uploads/2018/06/IMAG1504-e1529506431633.jpg' alt='Deer Camp' width='200px'/></center>";
+var deeropt =
 {
 'maxWidth': '500',
-'className' : 'custom6'
+'className' : 'custom4'
 }
-var marker6 = L.marker([51.49656, -0.76623], {icon: star}).bindPopup(customPopup6,customOptions6).addTo(map);
-// add two points to use for the runway
-var toUnion = [[51.495360, -0.775454],[51.503929, -0.768647]];
-// add the line between the points, make it light blue
-var polylineLayer = L.polyline(toUnion,{color:'lightblue',opacity:1}).addTo(map);
-// adding a circle around the center of the airfield, stroke color yellow, fill blue with low opacity
-var circle = L.circle([51.498163, -0.773273], 800,{color:'yellow',opacity:1,fillColor: 'yellow',fillOpacity:.2}).addTo(map);
+var deerr = L.marker([36.499634, -79.994164], {icon: deer}).bindPopup(deerpop,deeropt).addTo(map);
+
+//creates marker for 
+var pond = L.icon({
+iconUrl: 'https://www.pondexperts.ca/wp-content/uploads/2012/02/pond-consulting-hydrosphere.jpg', 
+iconSize: [50, 30], 
+popupAnchor: [0,-15] 
+});
+var pondpop = "<center><b>The Pond</b><br>Although this pond is not on the property, the owners of the pond allow my friend to use it for fishing.<br><img src='https://www.hakaimagazine.com/wp-content/uploads/header-fish-feel.jpg' alt='Fish' width='200px'/></center>";
+var pondopt =
+{
+'maxWidth': '500',
+'className' : 'custom4'
+}
+var pondd = L.marker([36.498250, -79.994406], {icon: pond}).bindPopup(pondpop,pondopt).addTo(map);
+
+
 // Define base layer
 var baseLayers = {
 "Base Map": ESRI
 // Define the historical overlay and markers
 };
 var overlays = {
-"White Waltham Airfield and Surrounding Area" : circle,
-"Aerial Image, WWII": Historical,
-"Runway 21": polylineLayer,
-"ATA Headquarters": marker1,
-"Runway Information": marker2,
-"Royal History": marker3,
-"Aircraft Tiedowns": marker4,
-"Maidenhead Office Park": marker5,
-"Waltham Road": marker6
+"My Friend's Property Boundaries" : poly,
+"Historical Orthoimagery, 2010": Historical,
+"Main Dirt Road": road,
+"The Cabin": cabinn,
+"The Northern Neighbor": mayoo,
+"The Southern Neighbor": deadd,
+"Cemetary": cemetaryy,
+"The Deer Camp": deerr,
+"The Neighbor's Pond": pondd
 };
 //add it all to the map!
 L.control.layers(baseLayers, overlays).addTo(map);
